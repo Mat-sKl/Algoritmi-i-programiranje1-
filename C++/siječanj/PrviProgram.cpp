a1b8817f59b5ecd9b2195b79b09d20f0e32bd658
Mat-sKl <mrmacketinak@gmail.com>
2025-01-11 18:43:36 +0100

    Add files via upload

---
+++ C++/siječanj/PrviProgram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int broj1;
+int broj2;
+int rezultat;
+cout<<"Ovaj program je napravljen za zbrajanje, oduzimanje, mnozenje i dijeljenje dva broja. Sve sto moras je upisati prvi i drugi broj, program ce napraviti sve ostalo."<<endl<<endl;
+cout<<"Unesi prvi broj: ";
+cin>>broj1;
+cout<<"Unesi drugi broj: ";
+cin>>broj2;
+cout<<endl;
+rezultat=broj1+broj2;
+cout<<"Zbroj brojeva= "<<rezultat<<endl;
+rezultat=broj1-broj2;
+cout<<"Razlika brojeva= "<<rezultat<<endl;
+rezultat=broj1*broj2;
+cout<<"Umnozak brojeva= "<<rezultat<<endl;
+rezultat=broj1/broj2;
+cout<<"Kolicnik brojeva= "<<rezultat<<endl;
+return 0;
+}
