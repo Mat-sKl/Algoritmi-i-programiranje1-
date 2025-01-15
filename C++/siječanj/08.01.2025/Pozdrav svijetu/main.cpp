e30d05d0d562588ad04a342d57252a2acd3128e5
Mat-sKl <mrmacketinak@gmail.com>
2025-01-15 10:07:34 +0100

    Add files via upload

---
+++ C++/siječanj/08.01.2025/Pozdrav svijetu/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+//ovo je jednoredni komentar
+
+/*  int(cijelibrojevi)
+    char(znakovi)
+    float(realni brojevi)
+    bool(istina/laž)
+    DEKLARACIJA
+*/
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//    return 0;
+//}
+//char znak='z';
+//deklaracija+inicijalizacija
+//tip naziv=poèetnaVrijednost
+//int a=0;
+/*{cout<<"Unesi broj: "<<endl;
+cin>>a;
+cout<<"Korisnik je unio broj: "<<endl;
+cout<<a<<endl}*/
+
+//varijabla je neka lokacija u memoriji
+
+//matematicke operacije + - * /
+//korisnik unosi 2 cijelobrojna broja
+// program ispise zbroj, razliku, umnozak, kolicnik ta dva broja
+int broj1;
+int broj2;
+int rezultat;
+cout<<"Unesi prvi broj: "<<endl;
+cin>>broj1;
+cout<<"Unesi drugi broj: "<endl;
+cin>>broj2;
+rezultat=broj+broj2;
+cout<<"Zbroj je"<<rezultat<<endl;
+rezultat=broj-broj2;
+cout<<"Razlika je"<<rezultat<<endl;
+rezultat=broj*broj2;
+cout<<"Umnožak je"<<rezultat<<endl;
+rezultat=broj/broj2;
+cout<<"Koliènik je"<<rezultat<<endl;
